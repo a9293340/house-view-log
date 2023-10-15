0f698ffa26688d2032d40693d3431abaf456b71f
Eric Hong <f102041332@gmail.com>
2023-10-15 23:35:59 +0800

    [fixed] house point info page

--- components/HousePointInfoPage/index.vue
+++ components/HousePointInfoPage/index.vue
@@ -1,7 +1,57 @@
-<script setup></script>
+<script setup>
+import { storeToRefs } from 'pinia';
+
+const { pickHousePoint } = storeToRefs(useMapStore());
+</script>
 
 <template>
-	<div>House Point Info</div>
+  <div class="house-point-info-page">
+    <div class="house-info-cards">
+      <img
+        :src="pickHousePoint.photos[0]"
+        v-if="pickHousePoint.photos.length"
+        class="house-view"
+        alt=""
+      />
+      <h1>{{ pickHousePoint.name }}</h1>
+      <div class="info-line">
+        <IconTemplate color="black" :size="20" icon="entypo-address" class="mr-2" />
+        <p>地址 :</p>
+        <span>{{ pickHousePoint.vicinity }}</span>
+      </div>
+      <div class="info-line">
+        <IconTemplate color="black" :size="20" icon="ep-coordinate" class="mr-2" />
+        <p>座標 :</p>
+        <span>{{ pickHousePoint.lat }},{{ pickHousePoint.lng }}</span>
+      </div>
+    </div>
+    <div class="house-forum-zone"></div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.house-point-info-page {
+  @apply w-full h-full flex flex-row lg:flex-col;
+  .house-info-cards {
+    @apply lg:mb-3 mr-2 lg:mr-0 lg:w-full w-5/12 h-76 lg:h-80 flex flex-col items-center rounded-lg box-border shadow-lg bg-white border border-gray-200 p-3;
+    .house-view {
+      @apply lg:h-44 lg:block hidden mb-2;
+    }
+    h1 {
+      @apply text-gray-700 font-bold text-2xl lg:text-3xl;
+    }
+    .info-line {
+      @apply w-full flex flex-row mt-2 items-center;
+      p {
+        @apply text-gray-500 font-semibold mr-2 text-base lg:text-xl;
+      }
+      span {
+        @apply text-congo-brown-500 font-medium text-base lg:text-xl;
+      }
+    }
+  }
+  .house-forum-zone {
+    @apply lg:w-full w-7/12 h-76 lg:h-[calc(100%-24rem)] overflow-auto scrollbar-thin scrollbar-track-rounded-lg;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,65 @@
 <script lang="ts" setup>
-const editorData = ref("<p>Content of the editor.</p>");
+const editorData = ref('<p>Content of the editor.</p>');
 const isShow = ref(false);
-const imgPath = ref("");
+const imgPath = ref('');
 
 const getEditData = () => console.log(editorData.value);
 
 const onChange = async (event: any) => {
-	const files = event.target.files;
-	const formData = new FormData();
-	formData.append("file", files[0]);
-	imgPath.value = (
-		await useFetch("/api/upload", {
-			method: "post",
-			body: formData,
-		})
-	).data.value?.path[0] as string;
+  const files = event.target.files;
+  const formData = new FormData();
+  formData.append('file', files[0]);
+  imgPath.value = (
+    await useFetch('/api/upload', {
+      method: 'post',
+      body: formData,
+    })
+  ).data.value?.path[0] as string;
 
-	console.log(imgPath.value);
+  console.log(imgPath.value);
 };
 
 onMounted(async () => {
-	isShow.value = true;
-	// const response = await $fetch("/upl/users/");
-	const local = await $fetch("/api/test");
-	// console.log(response);
-	console.log(local);
+  isShow.value = true;
+  // const response = await $fetch("/upl/users/");
+  const local = await $fetch('/api/test');
+  // console.log(response);
+  console.log(local);
 });
 </script>
 
 <template>
-	<div>
-		<h1>File Upload</h1>
-		<form>
-			<label for="file">File: </label>
-			<input type="file" name="file" @change="onChange" />
-		</form>
-		<img :src="imgPath" alt="" />
-	</div>
-	<div>
-		<div class="flex flex-row">
-			<el-button>Default</el-button>
-			<el-button type="primary" @click="getEditData">get Edit Data</el-button>
-			<el-button type="success">add Count</el-button>
-			<el-button type="info">Info</el-button>
-			<el-button type="warning">Warning</el-button>
-			<el-button type="danger">Danger</el-button>
-		</div>
-		<div class="flex flex-row">
-			I'm using an Iconify Icon!
-			<Icon icon="ic-baseline-search" class="w-36 h-36" />
-		</div>
-		<form>
-			<label for="locale-select">{{ $t("language") }}: </label>
-			<select id="locale-select" v-model="$i18n.locale">
-				<option value="en">en</option>
-				<option value="zh">zh</option>
-			</select>
-		</form>
-		<Tinymce v-model="editorData" v-if="isShow"></Tinymce>
-		<pre v-html="editorData"></pre>
-	</div>
+  <div>
+    <h1>File Upload</h1>
+    <form>
+      <label for="file">File: </label>
+      <input type="file" name="file" @change="onChange" />
+    </form>
+    <img :src="imgPath" alt="" />
+  </div>
+  <div>
+    <div class="flex flex-row">
+      <el-button>Default</el-button>
+      <el-button type="primary" @click="getEditData">get Edit Data</el-button>
+      <el-button type="success">add Count</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </div>
+    <div class="flex flex-row">
+      I'm using an Iconify Icon!
+      <Icon icon="ic-baseline-search" class="w-36 h-36" />
+    </div>
+    <form>
+      <label for="locale-select">{{ $t('language') }}: </label>
+      <select id="locale-select" v-model="$i18n.locale">
+        <option value="en">en</option>
+        <option value="zh">zh</option>
+      </select>
+    </form>
+    <Tinymce v-model="editorData" v-if="isShow"></Tinymce>
+    <pre v-html="editorData"></pre>
+  </div>
 </template>
 
 <style lang="scss"></style>
